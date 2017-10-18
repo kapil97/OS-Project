93513b329da23a6de5916281e7cfe20c8917a268
Kapil <kamleshwar.kamleshwar15@siesgst.ac.in>
2017-10-18 14:14:39 +0530

    First Commit

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gnome-terminal -e "java -jar Java_Text_Editor.jar" 
+echo "Welcome"
+sleep 2
+echo "Project Made by"
+echo "Kamleshwar Ragava 115A1080"
+echo "Siddhesh Shinde 115A1091"
+echo "Ashwathi Ashok 115A1097"
+echo "Fahad Shaikh 115A1085"
+
